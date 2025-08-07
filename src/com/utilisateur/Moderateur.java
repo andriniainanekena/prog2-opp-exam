f088aeadb86cf9392301c47af0159923c5d807aa
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-08-07 09:29:10 +0300

    chore: update Moderateur.java

--- src/com/utilisateur/Moderateur.java
+++ src/com/utilisateur/Moderateur.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Moderateur extends Utilisateur {
-    public Moderateur(String id, String nom, String email, LocalDate dateCreation,  List<Utilisateur> utilisateurs) {
-        super(id, nom, email, dateCreation, utilisateurs);
+    public Moderateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs, List<String> publication, List<String> commentaire) {
+        super(id, nom, email, dateCreation, utilisateurs, publication, commentaire);
     }
 
 }
