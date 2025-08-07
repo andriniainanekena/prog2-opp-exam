147ff8103c7000d4d18690f4a9fe5b01e50c4c53
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-08-07 09:49:35 +0300

    feat: add function supprimerCommentaires and supprimerPublications

--- src/com/utilisateur/Moderateur.java
+++ src/com/utilisateur/Moderateur.java
@@ -4,8 +4,30 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Moderateur extends Utilisateur {
-    public Moderateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs, List<String> publication, List<String> commentaire) {
-        super(id, nom, email, dateCreation, utilisateurs, publication, commentaire);
+    public Moderateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs, List<String> publications, List<String> commentaires) {
+        super(id, nom, email, dateCreation, utilisateurs, publications, commentaires);
     }
+
+    public boolean supprimerPublication(String publication) {
+        for(String publications: publication){
+            if(publications.contains(publication)){
+                publication.remove(publications);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean supprimerCommentaires(String commentaire) {
+        for(String commentaires: commentaire){
+            if(commentaires.contains(commentaire)){
+                commentaire.remove(commentaires);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
