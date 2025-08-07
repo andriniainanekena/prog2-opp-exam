86050b37437915a68a212069899378285b771fc9
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-08-07 09:28:42 +0300

    chore: update Administrateur.java

--- src/com/utilisateur/Administrateur.java
+++ src/com/utilisateur/Administrateur.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Administrateur extends Moderateur{
-    public Administrateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs) {
-        super(id, nom, email, dateCreation, utilisateurs);
+    public Administrateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs, List<String> publication, List<String> commentaire) {
+        super(id, nom, email, dateCreation, utilisateurs, publication, commentaire);
     }
 }
