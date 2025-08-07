e684c6ebf3d3193a5f3758257ec53cc072391ca0
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-08-07 09:54:36 +0300

    feat: add function supprimerUtilisateur

--- src/com/utilisateur/Administrateur.java
+++ src/com/utilisateur/Administrateur.java
@@ -4,7 +4,17 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Administrateur extends Moderateur{
-    public Administrateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs, List<String> publication, List<String> commentaire) {
-        super(id, nom, email, dateCreation, utilisateurs, publication, commentaire);
+    public Administrateur(String id, String nom, String email, LocalDate dateCreation, List<Utilisateur> utilisateurs, List<String> publications, List<String> commentaires) {
+        super(id, nom, email, dateCreation, utilisateurs, publications, commentaires);
+    }
+
+    public boolean supprimerUtilisateur(String utilisateur) {
+        for(Utilisateur utilisateurs: utilisateur) {
+            if(utilisateurs.contains(utilisateur)){
+                utilisateur.remove(utilisateurs);
+                return true;
+            }
+        }
+        return false;
     }
 }
